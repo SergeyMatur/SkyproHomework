561bf8e6155fcfeb6f318659cab4af5101095cca
Sergey_Stepanov <cmi52@mail.ru>
2022-05-07 17:42:32 +0300

    new

--- src/Homework/Peremennie.java
+++ src/Homework/Peremennie.java
@@ -1,4 +1,81 @@
 package Homework;
 
 public class Peremennie {
+    public static void main(String[] args) {
+        //задание 1
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        long d = 1;
+        float e = 1.1f;
+        double f = 1;
+        boolean g = a < 10;
+        char h = 35;
+        System.out.println(h);
+
+        //задание 2
+        float weight1 = 78.2f;
+        float weight2 = 82.7f;
+        float sum = weight1 + weight2;
+        float delta = weight1 - weight2;
+        System.out.println("Общий вес бойцов " +sum+" кг" );
+        System.out.println("Разница в весе бойцов" +delta+" кг" );
+
+        //задание 3
+        int banana = 5;
+        int bananaWeight = 80;
+        int bananaWeightAll = banana * bananaWeight;
+        int milk = 200;
+        int milkWight = 105/100;
+        int milkWightAll = milk * milkWight;
+        int iceCream = 2;
+        int iceCreamWeight = 100;
+        int iceCreamWeightAll = iceCream * iceCreamWeight;
+        int egg = 4;
+        int eggWeight = 70;
+        int eggWeightAll = egg * eggWeight;
+
+        int weightGramm = bananaWeightAll + milkWightAll + iceCreamWeightAll + eggWeightAll;
+        double weightKg = weightGramm/1000;
+        System.out.println("Вес завтрака "+weightGramm+ " грамм");
+        System.out.println("Вес завтрака "+weightKg+ " килограммграмм");
+
+        //задание 4
+        int excessWeight = 7000;
+        int result1 = 250;
+        int result2 = 500;
+        int result3 = (result1 + result2)/2;
+        int countDay1 = excessWeight / result1;
+        int countDay2 = excessWeight / result2;
+        int countDay3 = excessWeight / result3;
+        System.out.println("Если ежедневно худеть на "+ result1 + " грамм, на похудение уйдет " + countDay1+ " дней");
+        System.out.println("Если ежедневно худеть на "+ result2 + " грамм, на похудение уйдет " + countDay2+ " дней");
+        System.out.println("Если ежедневно худеть на "+ result3 + " грамм, на похудение уйдет " + countDay3+ " дней");
+
+        //задание 5
+        double increase = 1.1;
+        double month = 12;
+
+
+        double salaryMasha = 67760;
+        double salaryDenis = 83690;
+        double salaryKristina = 76230;
+
+        double salaryMashaYear = salaryMasha * month;
+        double salaryDenisYear = salaryDenis * month;
+        double salaryKristinaYear = salaryKristina * month;
+
+        double salaryMashaNew = salaryMasha * increase;
+        double salaryDenisNew = salaryDenis * increase;
+        double salaryKristinaNew = salaryKristina * increase;
+
+        double salaryMashaNewYear = salaryMashaNew * month;
+        double salaryDenisNewYear = salaryDenisNew * month;
+        double salaryKristinaNewYear = salaryKristinaNew * month;
+
+        System.out.println("Маша теперь получает " + salaryMashaNew + " рублей. Годовой доход вырос на " + (salaryMashaNewYear - salaryMashaYear) + " рублей");
+        System.out.println("Денис теперь получает " + salaryDenisNew + " рублей. Годовой доход вырос на " + (salaryDenisNewYear - salaryDenisYear) + " рублей");
+        System.out.println("Кристина теперь получает " + salaryKristinaNew + " рублей. Годовой доход вырос на " + (salaryKristinaNewYear - salaryKristinaYear) + " рублей");
+    }
+
 }
--- src/Homework/UslovnieOperatory.java
+++ src/Homework/UslovnieOperatory.java
@@ -1,4 +1,100 @@
 package Homework;
 
 public class UslovnieOperatory {
+    public static void main(String[] args) {
+
+        //Задание 1
+        int clientOS = 0;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+
+        //Задание 2
+        int clientDeviceYear = 2016;
+        if (clientDeviceYear <= 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облнгченную версию приложения для iOS по ссылке");
+            }
+            if (clientOS == 1) {
+                System.out.println("Установите облнгченную версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Установите приложение по ссылке");
+
+        }
+
+        //Задание 3
+        int year = 2024;
+        if (year % 4 == 0 || year % 400 == 0) {
+            if (year % 100 == 0) {
+                System.out.println("Год не высокостный");
+            }
+            if (year % 100 != 0) {
+                System.out.println("Год высокостный");
+            }
+        } else {
+            System.out.println("Год не высокостный");
+        }
+
+        //Задание 4
+        int deliveryDistance = 110;
+        int day = 0;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + (day + 1));
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: " + (day + 2));
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: " + (day + 3));
+        } else {
+            System.out.println("Так далеко не доставим");
+        }
+
+        //Задание 5
+        int monthNumber = 6;
+
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
+    }
 }
